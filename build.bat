19817fe2b1a2420d480a5610c9d70fc43ad9442f
Matthias Kauer <mk.software@zuez.org>
2015-07-21 11:19:57 +0800

    batch now working

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
-call "g++ -c -fPIC foo.cpp -o foo.o"
-ECHO "batch still running"
-g++ -shared -Wl,-soname,libfoo.so -o libfoo.so  foo.o
+call g++ -c foo.cpp -o foo.o
+call g++ -shared -Wl,-soname,libfoo.so -o libfoo.so  foo.o
