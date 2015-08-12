9a4ce968a9dd4b5889c2c1d3eb34f3e33b7ee26b
Matthias Kauer <mk.software@zuez.org>
2015-08-13 06:03:20 +0800

    with init file, I can call swig compilation successfully even though lib is in root dir

--- miniextension_test.py
+++ miniextension_test.py
@@ -1,2 +1,5 @@
 import cos_module
-print("calling cos_module", cos_module.cos_func(1.0))
+print("calling Python extension cos_module", cos_module.cos_func(1.0))
+
+import cswig_cos.cswig_cos as cswig
+print(cswig.cos_func(0.0))
