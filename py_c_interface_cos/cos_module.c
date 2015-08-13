f562c08a4b8dc1b6b46669a767e18c5a090657d9
Matthias Kauer <mk.software@zuez.org>
2015-08-13 08:46:34 +0800

    added python3 module def to c interface

--- py_c_interface_cos/cos_module.c
+++ py_c_interface_cos/cos_module.c
@@ -4,6 +4,7 @@
 #include <Python.h>
 #include <math.h>
 
+
 /*  wrapped cosine function */
 static PyObject* cos_func(PyObject* self, PyObject* args)
 {
@@ -30,11 +31,31 @@ static PyMethodDef CosMethods[] =
      {"cos_func", cos_func, METH_VARARGS, "evaluate the cosine"},
      {NULL, NULL, 0, NULL}
 };
+#if PY_MAJOR_VERSION >= 3
+//see http://python3porting.com/cextensions.html for porting
+    static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "cos_module",     /* m_name */
+        "This is a module",  /* m_doc */
+        -1,                  /* m_size */
+        CosMethods,    /* m_methods */
+        NULL,                /* m_reload */
+        NULL,                /* m_traverse */
+        NULL,                /* m_clear */
+        NULL,                /* m_free */
+    };
+#endif
 
 /* module initialization */
 PyMODINIT_FUNC
 
+#if PY_MAJOR_VERSION >= 3
+PyInit_cos_module(void){
+    return PyModule_Create(&moduledef);
+}
+#else
 initcos_module(void)
 {
      (void) Py_InitModule("cos_module", CosMethods);
 }
+#endif
