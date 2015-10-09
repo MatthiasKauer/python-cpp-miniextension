64d335ca4dd0c6ce97c13ab089f2799af6a397b7
Matthias Kauer <mk.software@zuez.org>
2015-10-09 16:58:40 +0800

    added fact to setup.py and test script

--- script/miniextension_test.py
+++ script/miniextension_test.py
@@ -1,6 +1,9 @@
 import cos_module
-
 import cswig_cos.cswig_cos as cswig
+import cppswig_factorial.ccpswig_factorial as cppfact
+
+
 def main():
     print("calling Python extension cos_module", cos_module.cos_func(1.0))
     print(cswig.cos_func(0.0))
+    print(cppfact.factorial(8)
--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
         # Extension('py_ctypes_foo_module', ['py_ctypes_foo/foo.cpp']),
         Extension('cos_module', ['py_c_interface_cos/cos_module.c']),
         Extension('_cswig_cos', ['cswig_cos/cos_wrap.c', 'cswig_cos/cos.c']),
+        Extension('_fact', ['cppswig_factorial/factorial_wrap.cxx', 'cppswig_factorial/factorial.cpp'],
+                  extra_compile_args=['-std=c++11']),
      ],
     packages = packages,
     entry_points={'console_scripts': ['miniextension_test=script.miniextension_test:main']}
