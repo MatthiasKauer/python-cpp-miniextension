066c692fa8c7ead738f60c651cae6355050db7d7
Matthias Kauer <mk.software@zuez.org>
2015-10-12 09:49:27 +0800

    forgot adding setup and test script to commit

--- mylib/script/miniextension_test.py
+++ mylib/script/miniextension_test.py
@@ -1,9 +1,13 @@
 import cos_module
 import mylib.cswig_cos.cswig_cos as cswig
-import mylib.cppswig_factorial.fact as cppfact
-
+import mylib.cppswig_stack.mystack as cppmystack
 
 def main():
     print("calling Python extension cos_module", cos_module.cos_func(1.0))
     print("Callign C swig module: ", cswig.cos_func(0.0))
-    print("Calling C++11 swig module: ", cppfact.factorial(8))
+    #  print("Calling C++11 swig module: ", cppfact.factorial(8))   #doesn't work
+    print("Callign C++ (not 11!!) swig module: ")
+    ms = cppmystack.intMystack();
+    for i in range(7):
+        ms.put(i)
+    ms.print_stack()
